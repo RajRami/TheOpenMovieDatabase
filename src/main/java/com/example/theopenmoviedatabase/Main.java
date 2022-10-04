54cf373d4d406345908818806ca5f97eff52566f
Raj <200489571@student.georgianc.on.ca>
2022-10-04 15:45:26 -0400

    Updated the logo of the application

--- src/main/java/com/example/theopenmoviedatabase/Main.java
+++ src/main/java/com/example/theopenmoviedatabase/Main.java
@@ -3,6 +3,7 @@ package com.example.theopenmoviedatabase;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,6 +13,8 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("search-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+        Image img = new Image(getClass().getResourceAsStream("/Movie.jpg"));
+        stage.getIcons().add(img);
         stage.setTitle("Play With Our Movie Database");
         stage.setScene(scene);
         stage.show();
